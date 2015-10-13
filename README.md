0f286d5a54a070a5c1f57f1a0d105a478b75cbb7
Jordan Skole <jordan@usebenchmark.com>
2015-10-13 00:44:27 -0400

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# detroitstartupjobs
-yet another jobs board. 
--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('detroitstartupjobs', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
-
-})();
