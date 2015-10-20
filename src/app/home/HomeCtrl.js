8a5173ed1e669f11f3cf06d8fabb4dd76ab42502
Jordan Skole <jordan@usebenchmark.com>
2015-10-20 19:14:31 -0400

    simple list of items on home page

--- src/app/home/HomeCtrl.js
+++ src/app/home/HomeCtrl.js
@@ -8,6 +8,25 @@
   /** @ngInject */
   function HomeCtrl($scope) {
     $scope.data = "Hello World"
+
+    $scope.jobs = [
+      {
+        "title": "Campus Director",
+        "created_on": "Friday",
+        "salary": 88000,
+        "employer": "The Iron Yard",
+        "location": "Detroit",
+        "skills": ["JavaScript", "iOS", "Android", "Adobe Illustrator"]
+      },
+      {
+        "title": "Post Production Coordinator",
+        "created_on": "Wed",
+        "salary": 30000,
+        "employer": "Heather Read Photography & Design",
+        "location": "Royal Oak",
+        "skills": ["Adobe Photoshop"]
+      }
+    ]
   }
 
 })();
