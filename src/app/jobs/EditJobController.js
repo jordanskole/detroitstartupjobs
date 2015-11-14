d2987e4add7634cd8fe2dfe0a103f4517a3954a6
Jordan Skole <jordan@usebenchmark.com>
2015-11-14 12:27:28 -0500

    select a company and update the job company_id

--- src/app/jobs/EditJobController.js
+++ src/app/jobs/EditJobController.js
@@ -6,13 +6,22 @@
     .controller('EditJobController', EditJobController);
 
   /** @ngInject */
-  function EditJobController ($log, $scope, $stateParams, Jobs, CurrentAuth, Accounts) {
+  function EditJobController ($log, $scope, $stateParams, Jobs, CurrentAuth, Accounts, Companies) {
 
     var vm = this;
+    $scope.detail = {
+      skills: []
+    };
     Jobs
       .$object($stateParams.id)
       .$bindTo($scope, 'detail');
 
+    // get our companies for autocomplete
+    vm.companies = Companies.$array;
+    vm.selectedCompanyChange = function () {
+      $scope.detail.company_id = vm.selectedCompany.$id;
+      $log.log($scope.detail.company_id);
+    }
   }
 
 })();
